4ae678a3f9ff0b83b4665c52b6f042ab4dd1842d
Sharvansamala <sharvansamala2002@gmail.com>
2025-08-24 15:04:22 -0500

    add JWT utility class for token generation and validation

---
+++ user-service/src/main/java/com/paypal/user_service/utils/JWTUtil.java
@@ -0,0 +1,53 @@
+package com.paypal.user_service.utils;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.Map;
+
+@Component
+public class JWTUtil {
+
+    private static final String SECRET = "adfnkjadnsfjnadsjfadsnfjnasdfnkasdnfalsdfnasdj";
+
+    private Key getSigningKey() {
+        return Keys.hmacShaKeyFor(SECRET.getBytes());
+    }
+
+    private String extractEmail(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token, String email) {
+        String tokenEmail = extractEmail(token);
+        return (tokenEmail.equals(email));
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public String generateToken(Map<String,Object> claims, String email) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(email)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+86400000)) // 1 day
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+}
